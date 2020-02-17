7181d5b5ddb3b8bdf03dcf8b217c171a09f6f8c4
BrazhnikovV <brazhnikov_ve@mail.ru>
2020-02-17 17:03:45 +0300

    Reverse spiral

--- src/main/java/Main2.java
+++ src/main/java/Main2.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Main2 {
 
     public static void main(String[] args) {
 
@@ -9,26 +9,27 @@ public class Main {
         int max = row * col;
 
         while (count < max) {
-            for (int i = minCol; i <= maxCol; i++) {
-                array[minRow][i] = count;
-                count++;
-            }
-            minRow = minRow + 1;
             for (int i = minRow; i <= maxRow; i++) {
-                array[i][maxCol] = count;
+                array[i][minCol] = count;
                 count++;
             }
-            maxCol = maxCol - 1;
-            for (int i = maxCol; i >= minCol; i--) {
+            minCol = minCol + 1;
+            for (int i = minCol; i <= maxCol; i++) {
                 array[maxRow][i] = count;
                 count++;
             }
             maxRow = maxRow - 1;
             for (int i = maxRow; i >= minRow; i--) {
-                array[i][minCol] = count;
+                array[i][maxCol] = count;
                 count++;
             }
-            minCol = minCol + 1;
+            maxCol = maxCol - 1;
+
+            for (int i = maxCol; i >= minCol; i--) {
+                array[minRow][i] = count;
+                count++;
+            }
+            minRow = minRow + 1;
         }
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
